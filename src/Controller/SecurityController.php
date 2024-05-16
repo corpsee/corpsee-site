c683b0d9c53f39853d65e1a2d36a26d0bbc8b429
Dmitry Khomutov <poisoncorpsee@gmail.com>
2024-05-16 23:15:16 +0700

    Improved login page.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,16 +14,34 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+            // parameters usually defined in Symfony login forms
+            'error' => $error,
+            'last_username' => $lastUsername,
+
+            // OPTIONAL parameters to customize the login form:
+
+            // the title visible above the login form (define this option only if you are
+            // rendering the login template in a regular Symfony controller; when rendering
+            // it from an EasyAdmin Dashboard this is automatically set as the Dashboard title)
+            'page_title' => 'Corpsee Site Login',
+
+            // the string used to generate the CSRF token. If you don't define
+            // this parameter, the login form won't include a CSRF token
+            'csrf_token_intention' => 'authenticate',
+
+            // the URL users are redirected to after the login (default: '/admin')
+            'target_path' => $this->generateUrl('admin'),
+
+            // whether to enable or not the "remember me" checkbox (default: false)
+            'remember_me_enabled' => true,
+
+            // whether to check by default the "remember me" checkbox (default: false)
+            'remember_me_checked' => true,
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
